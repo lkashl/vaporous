a57c745e3aa1c104cd36cc6c67a2c0fdb73c1460
Kash <lkashl@hotmail.com>
2025-10-03 00:30:25 +1000

    - Prepare for version 0.0.6

--- scripts/publish_pages.sh
+++ scripts/publish_pages.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # checks if there are any uncommitted changes or unstaged/staged changes in the git repo
-# if there are no changes, checks out the 'pages' branch
-# supports DRY_RUN=1 environment variable to only print actions
+# if there are no changes, will proceed with publish activities
 
 set -euo pipefail
 
-DRY_RUN=${DRY_RUN:-0}
 REPO_ROOT="$(git rev-parse --show-toplevel 2>/dev/null || echo "")"
 
 if [ -z "$REPO_ROOT" ]; then
@@ -41,3 +39,5 @@ git commit -m "- Update pages"
 git push
 
 git checkout main
+npm version patch
+npm publish
